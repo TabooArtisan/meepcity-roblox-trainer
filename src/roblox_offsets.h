95e5474ffa2fc92d605c8633f4ab9a0b895ab0d7
TabooArtisan <danieldavis2501600@gmail.com>
2026-05-30 23:01:36 -0400

    feat: add src/roblox_offsets.h

---
+++ src/roblox_offsets.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstdint>
+
+namespace Offsets {
+    constexpr uintptr_t LocalPlayer = 0x1A38908;
+    constexpr uintptr_t Character = 0x7C;
+    constexpr uintptr_t Health = 0x1A4;
+}
